2effe6cd119d0736deee21ebd2c2530b21b90e43
SHUBHAM TIWARI <98177201+rocksLionstar@users.noreply.github.com>
2022-09-14 16:23:26 +0530

    changed backend url

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const Car = require('./models/carModel');
 const port = process.env.PORT || 5000;
 const dbConnection = require('./db');
-var cors = require('cors')
+const cors = require('cors')
 app.use(express.json());
 const path = require("path");
 const usersRoute = require('./routes/usersRoute')
@@ -11,11 +11,17 @@ const carsRoute = require('./routes/carsRoute')
 const bookingsRoute = require('./routes/bookingsRoute')
 
 
-app.use(cors())
 app.use("/api/cars/", require("./routes/carsRoute"));
 app.use("/api/users/", require("./routes/usersRoute"));
 app.use("/api/bookings/", require("./routes/bookingsRoute"));
-
+app.use(
+  cors({
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: ['http://localhost:3000', 'https://strong-zabaione-96e33f.netlify.app'], // whatever ports you used in frontend
+  })
+);
 
 //----------deployment-----------------
 
